bf8f42904c97fff63a1b505ebda77f9b9951781d
tterrag <tterrag1098@gmail.com>
2019-05-29 01:59:37 -0400

    Begin portfolio

---
+++ portfolio/index.php
@@ -0,0 +1,81 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+	<link rel="stylesheet" type="text/css" href="../root.css">
+	<link rel="stylesheet" type="text/css" href="portfolio.css">
+
+  <title>tterrag - Portfolio</title>
+  <meta name="author" content="tterrag">
+	<meta name="description" content="Portfolio">
+	<meta name="theme-color" content="#4e9745">
+
+	<meta property="og:title" content="Portfolio">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="http://www.tterrag.com/portfolio">
+	<meta property="og:site_name" content="tterrag.com">
+
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:title" content="Portfolio">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+<header>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/k9">K9</a>
+		<a href="/lang2json">Lang to JSON Converter</a>
+		<span class="separator"></span>
+		<a href="https://github.com/tterrag1098/">GitHub</a>
+		<a href="https://ci.tterrag.com/">Jenkins</a>
+  </nav>
+</header>
+<body>
+  <div id="mobile-wrapper">
+  <div id="content">
+		<h1>Portfolio</h1>
+		<h2>Mods</h2>
+		<p>
+			Minecraft mods that I have created, or had a large part in creating.
+		</p>
+		<div class="project mod">
+			<div class="header">
+				<h3>Chisel</h3> &mdash; Project Manager, Developer <span class="project-links"><a href="https://minecraft.curseforge.com/projects/chisel">CurseForge</a> | <a href="https://github.com/Chisel-Team/Chisel/">GitHub</a> | <a href="https://www.youtube.com/watch?v=nzUShh3WrPk">YouTube Video</a>
+			</div>
+			<div class="desc">
+				<div class="text with-image">
+					<p>
+						Chisel adds a massive amount of new decorative blocks to the game, most of which are variants on existing blocks. It utilizes a unique crafting method, the chisel, to choose between the myriad options available to the player. Many of the blocks use advanced rendering, colloquially known as "CTM", or connected textures.
+					</p><p>
+						I began working on the Chisel project in early 2015, mainly to do bug fixing and quality-of-life changes. Eventually, I became the main developer on the project, and along with Drullkus and minecreatr have maintained the mod since then, including a full rewrite for Minecraft 1.8+. Since then, the sister mod CTM was born when I realized the rendering part would be useful to other modders, and artists in general.
+					</p><p>
+						Since August 2015, Chisel has been downloaded over 40 million times from CurseForge.
+					</p>
+				</div>
+				<img class="image" src="chisel.png" />
+			</div>
+		</div>
+
+		<div class="project mod">
+			<div class="header">
+				<h3>CTM</h3> &mdash; Creator <span class="project-links"><a href="https://minecraft.curseforge.com/projects/ctm">CurseForge</a> | <a href="https://github.com/Chisel-Team/ConnectedTexturesMod/">GitHub</a> | <a href="https://github.com/Chisel-Team/ConnectedTexturesMod/">Wiki</a>
+			</div>
+			<div class="desc">
+				<div class="text with-image">
+					<p>
+						Connected Textures Mod is a utility for artists. It extends the Minecraft model and texture metadata formats with additional capabilities, such as connected textures, patterns, edge blending, and emissivity. It requires no code to be used, only edits to the JSON files that make up a resource pack.
+					</p>
+					<p>
+						CTM is used by mods to enhance their blocks, such as <a href="https://minecraft.curseforge.com/projects/the-twilight-forest">Twilight Forest</a>, <a href="https://minecraft.curseforge.com/projects/immersive-engineering">Immersive Engineering</a>, and <a href="https://minecraft.curseforge.com/projects/mekanism">Mekanism</a>, as well as many more. Since May 2017, it has been downloaded over 22 million times from CurseForge.
+					</p>
+				</div>
+				<img class="image" src="ctm.png" />
+			</div>
+		</div>
+	</div>
+	</div>
+</body>
