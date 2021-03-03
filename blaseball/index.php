bc40dc992fa3c970ced2b063cc7a9e2d29093020
tterrag1098 <tterrag1098@gmail.com>
2021-03-03 01:15:41 -0500

    fix php include

--- blaseball/index.php
+++ blaseball/index.php
@@ -47,9 +47,7 @@
         </p>
         <details>
             <summary style="cursor:pointer;">Expand to view the full code.<br/><br/>This is a small script that is purely clientside. It collects no information and makes no network requests.</summary>
-            <pre>
-                <?= file_get_contents('blaesball-dashboard.user.js') ?>
-            </pre>
+            <pre><?= htmlspecialchars(file_get_contents('blaseball-dashboard.user.js')) ?></pre>
         </details>
     </div>
     </div>
